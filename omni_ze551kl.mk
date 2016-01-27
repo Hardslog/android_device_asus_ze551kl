d735231a6f17dfe0fc017a349a0fb32c767c485a
Joshua Lynch <jl52741@gmail.com>
2016-01-27 06:32:35 -0600

    WIP: TWRP for the ze551kl

---
+++ omni_ze551kl.mk
@@ -0,0 +1,13 @@
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/asus/ze551kl/device.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := omni_ze551kl
+PRODUCT_DEVICE := ze551kl
+PRODUCT_BRAND := ASUS
+PRODUCT_MODEL := ZE551KL
+PRODUCT_MANUFACTURER := ASUS
+
