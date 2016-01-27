bbbe4c81c069e378d9d79e4451a408729963ef31
Joshua Lynch <jl52741@gmail.com>
2016-01-27 08:03:39 -0600

    We are building for a phone.

--- omni_ze551kl.mk
+++ omni_ze551kl.mk
@@ -1,5 +1,5 @@
 # Inherit from the common Open Source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/asus/ze551kl/device.mk)
